f15af0c91b6aefde4a86a17b5852f984db622e92
jakesolomon <jacob_solomon@student.uml.edu>
2019-12-10 09:02:08 -0500

    No Live Data
    Using static data in a constant to feed SAT data.

--- app/javascript/react/containers/QueryPage.js
+++ app/javascript/react/containers/QueryPage.js
@@ -17,34 +17,42 @@ class QueryPage extends Component {
     };
   }
 
-  componentDidMount() {
-    // FETCH ALL TESTS
-    fetch("http://localhost:3000/api/v1/tests")
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        let errorMessage = `${response.status} (${response.statusText})`,
-          error = new Error(errorMessage);
-        throw(error);
-      }
-    })
-    .then(response => response.json())
-    .then(body => {
-      let data = [
-        {x: "800–1199", y: body.low},
-        {x: "1200–1399", y: body.mid},
-        {x: "1400–1499", y: body.high},
-        {x: "1500–1600", y: body.veryHigh}
-      ];
-      this.setState( { tests: data } );
-    })
-    .catch(error => console.error(`Error in fetch: ${error.message}`));
-  }
+  // componentDidMount() {
+  //   // FETCH ALL TESTS
+  //   fetch("http://localhost:3000/api/v1/tests")
+  //   .then(response => {
+  //     if (response.ok) {
+  //       return response;
+  //     } else {
+  //       let errorMessage = `${response.status} (${response.statusText})`,
+  //         error = new Error(errorMessage);
+  //       throw(error);
+  //     }
+  //   })
+  //   .then(response => response.json())
+  //   .then(body => {
+  //     let data = [
+  //       {x: "800–1199", y: body.low},
+  //       {x: "1200–1399", y: body.mid},
+  //       {x: "1400–1499", y: body.high},
+  //       {x: "1500–1600", y: body.veryHigh}
+  //     ];
+  //     this.setState( { tests: data } );
+  //   })
+  //   .catch(error => console.error(`Error in fetch: ${error.message}`));
+  // }
 
 // TODO: Build function to send to QueryParams to collect data about user actions.
 
   render() {
+
+    const SATDATA = [
+      {x: "800–1199", y: 109.7},
+      {x: "1200–1399", y: 106.6},
+      {x: "1400–1499", y: 72.7},
+      {x: "1500–1600", y: 38}
+    ];
+
     return(
       <div>
         <h2 className="main-header">Query</h2>
@@ -53,7 +61,7 @@ class QueryPage extends Component {
             <QueryParams />
           </div>
           <div className="cell medium-6 large-8 query-display">
-            <BarGraph data={this.state.tests}/>
+            <BarGraph data={SATDATA}/>
           </div>
         </div>
       </div>
