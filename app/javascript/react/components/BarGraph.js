4f10be4de1a770dda208e1499d42deffc0acb3e1
jakesolomon <jacob_solomon@student.uml.edu>
2020-01-02 21:36:24 -0600

    Added mouseOver interaction
    Mouse over graph shows exact value of bar graph. Had to make BarGraph stateful component.

--- app/javascript/react/components/BarGraph.js
+++ app/javascript/react/components/BarGraph.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 import '../../../../node_modules/react-vis/dist/style.css';
 import {
   XYPlot,
@@ -10,92 +11,150 @@ import {
   Hint
 } from 'react-vis';
 
-const BarGraph = (props) => {
+class BarGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoveredNode: {x: null, y: null},
+      hint: false
+    };
+    this.changeHint = this.changeHint.bind(this);
+  }
 
-  let seriesColors = [
-    "00EE00",
-    "EE0000"
-  ];
-
-  let barSeries = props.data.data.map(data => {
-    return(
-      <VerticalBarSeries
-      data={data}
-      key={props.data.data.indexOf(data)}
-      color={seriesColors[props.data.data.indexOf(data)]}
-      />
-    );
-  });
-
-  let legendItems = [
-    {
-      title: props.data.legend[0],
-      color: seriesColors[0]
+  changeHint(d) {
+    if (this.state.hoveredNode.x != d.x || this.state.hoveredNode.y != d.y) {
+      this.setState({
+        hoveredNode: {
+          x: d.x, y: Math.round(d.y*10)/10
+        },
+        hint: true
+      });
     }
-  ];
+  }
 
-  if (props.data.data[1]) {
-    legendItems.push(
+  render() {
+    const hoveredNode = this.state;
+
+    let seriesColors = [
+      "00EE00",
+      "EE0000"
+    ];
+
+    let barSeries = this.props.data.data.map(data => {
+      return(
+        <VerticalBarSeries
+        changeHint={this.changeHint}
+        data={data}
+        key={this.props.data.data.indexOf(data)}
+        color={seriesColors[this.props.data.data.indexOf(data)]}
+        onValueMouseOver={(d,e) => {
+          this.changeHint(d);
+        }}
+        />
+      );
+    });
+
+    let legendItems = [
       {
-        title: props.data.legend[1],
-        color: seriesColors[1]
+        title: this.props.data.legend[0],
+        color: seriesColors[0]
       }
+    ];
+
+    if (this.props.data.data[1]) {
+      legendItems.push(
+        {
+          title: this.props.data.legend[1],
+          color: seriesColors[1]
+        }
+      );
+    }
+
+    let visibility = "";
+    if (!this.props.showStudentCount) {
+      visibility = "disappear";
+    }
+
+    let hideHint;
+    if (this.state.hoveredNode.x == null) {
+      hideHint = "disappear";
+    } else {
+      hideHint = "";
+    }
+
+    return (
+      <div className="barGraph row">
+      <h4 className="barGraph-header">{this.props.data.title}</h4>
+      <XYPlot
+      className="cell large-9"
+      margin={{top: 40, left: 70, bottom: 45}}
+      xType="ordinal"
+      width={700}
+      height={300}
+      animation={true}
+      onMouseLeave={() => this.setState({hoveredNode: {x: null, y: null} })}
+      >
+      <DiscreteColorLegend
+      className="cell large-3"
+      items={legendItems}
+      orientation="horizontal"
+      />
+      {barSeries}
+      <XAxis
+      animation={false}
+      tickSize={0}
+      style={{ text: {fontSize: 14} }}
+      />
+      <XAxis
+      title={this.props.data.xLabel}
+      style={{ line: {stroke:'none'} }}
+      animation={false}
+      top={303}
+      />
+      <XAxis
+      tickFormat={v => this.props.data.studentCount.map(count => {
+        return (count[v]);
+      }).join(" | ")}
+      top={0}
+      style={{ line: {stroke:'none'}, text: {fontSize: 14} }}
+      animation={false}
+      className={visibility}
+      />
+      <YAxis
+      style={{ text: {fontSize: 14} }}
+      />
+      <YAxis
+      title={this.props.data.yLabel}
+      style={{ line: {stroke:'none'}, text: {fill: 'none'} }}
+      left={-65}
+      />
+      {hoveredNode && (
+            <Hint
+              xType="ordinal"
+              yType="literal"
+              getX={d => d.x}
+              getY={d => d.y}
+              value={{
+                x: this.state.hoveredNode.x,
+                y: this.state.hoveredNode.y,
+              }}
+              className={hideHint}
+              format={d => {
+                let hint=[];
+                if (d.y) {
+                  hint.push(
+                    {title: `Value`, value: d.y}
+                  );
+                }
+                console.log(hint);
+                return(hint);
+              }}
+            />
+      )}
+      </XYPlot>
+      </div>
     );
   }
-
-  let visibility = "";
-  if (!props.showStudentCount) {
-    visibility = "disappear";
-  }
-
-  return (
-    <div className="barGraph row">
-      <h4 className="barGraph-header">{props.data.title}</h4>
-      <XYPlot
-        className="cell large-9"
-        margin={{top: 40, left: 70, bottom: 45}}
-        xType="ordinal"
-        width={700}
-        height={300}
-        animation={true}
-      >
-        <DiscreteColorLegend
-        className="cell large-3"
-        items={legendItems}
-        orientation="horizontal"
-        />
-        {barSeries}
-        <XAxis
-        animation={false}
-        tickSize={0}
-        style={{ text: {fontSize: 14} }}
-        />
-        <XAxis
-        title={props.data.xLabel}
-        style={{ line: {stroke:'none'} }}
-        animation={false}
-        top={303}
-        />
-        <XAxis
-        tickFormat={v => props.data.studentCount.map(count => {
-          return (count[v]);
-        }).join(" | ")}
-        top={0}
-        style={{ line: {stroke:'none'}, text: {fontSize: 14} }}
-        animation={false}
-        className={visibility}
-        />
-        <YAxis
-        style={{ text: {fontSize: 14} }}
-        />
-        <YAxis
-        title={props.data.yLabel}
-        style={{ line: {stroke:'none'}, text: {fill: 'none'} }}
-        left={-65}
-        />
-      </XYPlot>
-    </div>
-  );
-};
+}
 
 export default BarGraph;
